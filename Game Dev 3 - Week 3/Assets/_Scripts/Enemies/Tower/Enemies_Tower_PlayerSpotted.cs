09d396f06213f7f20e5aae6d11121b1de1f0e414
calumyau <calumyau2005@gmail.com>
2024-10-09 16:36:43 +0100

    Enemy Spotter

--- Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
+++ Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
@@ -1,3 +1,4 @@
+using GameDevWithCalum.ObserverPattern;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,15 @@ namespace GameDevWithCalum.Enemies
 
     public class Enemies_Tower_PlayerSpotted : MonoBehaviour
     {
+        [SerializeField] GameEvent playerSpotted;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             //If the player enters the collider, the spotted event is raised!
             if (collision.gameObject.tag == "Player")
             {
-                //Do something
+
+
+              playerSpotted?.Raise();
             }
         }
     }
